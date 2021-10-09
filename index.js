508cc15751534c829f4560b6298f4b2910d5e3f3
Peter.Li <peter.li@pushpay.com>
2021-10-09 19:26:00 +1300

    feat: 🎸 add readline in CMD

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
+readline.question(`your name?`, (name) => {
+  console.log(`your name is ${name}!`);
+  readline.close();
+});
