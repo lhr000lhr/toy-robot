a306d49615f6273658ff9f3488fd562dba0e2e20
Peter.Li <peter.li@pushpay.com>
2021-10-10 23:31:53 +1300

    test: 💍 add exception test cases for inputToState

--- utils/__tests__/inputToState.text.js
+++ utils/__tests__/inputToState.text.js
@@ -1,15 +1,37 @@
+const chalk = require("chalk");
+
 const inputToState = require("../inputToState");
 
-test("convert Input to initial state", () => {
-  expect(inputToState("0,0,NORTH")).toEqual({
-    direction: "NORTH",
-    x: 0,
-    y: 0,
+describe("convert Input to initial state", () => {
+  test("should convert correctly", () => {
+    expect(inputToState("0,0,NORTH")).toEqual({
+      direction: "NORTH",
+      x: 0,
+      y: 0,
+    });
+
+    expect(inputToState("0,0,SOUTH")).toEqual({
+      direction: "SOUTH",
+      x: 0,
+      y: 0,
+    });
   });
 
-  expect(inputToState("0,0,SOUTH")).toEqual({
-    direction: "SOUTH",
-    x: 0,
-    y: 0,
+  test("should throw exception when receiving illegal parameter", () => {
+    const parameters = [
+      "0,a,SOUTH",
+      "0,7,SOUTH",
+      "0,,SOUTH",
+      "0,:,SOdUTH",
+      "0,0,0",
+      null,
+      undefined,
+      "----"
+    ];
+    parameters.forEach((parameter) => {
+      expect(() => {
+        inputToState(parameter);
+      }).toThrowError(`input parameter ${chalk.red(parameter)} error`);
+    });
   });
 });
--- utils/inputToState.js
+++ utils/inputToState.js
@@ -1,4 +1,12 @@
+const chalk = require("chalk");
+
+const pattern = /-?[0-4],-?[0-4],\b(NORTH|SOUTH|EAST|WEST)\b/;
+
 const inputToState = (input) => {
+  if (!pattern.test(input)) {
+    throw `input parameter ${chalk.red(input)} error`
+  }
+
   const [x, y, direction] = input.split(",");
 
   return {
